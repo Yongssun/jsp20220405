9fed374115da82913438cdc2ab24772cfe5d0997
Yongssun <99157768+Yongssun@users.noreply.github.com>
2022-04-21 16:48:33 +0900

    0421

--- src/chap14/S14Servlet01.java
+++ src/chap14/S14Servlet01.java
@@ -1,16 +1,26 @@
 package chap14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.jstl.sql.Result;
+import javax.sql.DataSource;
 
-import com.sun.corba.se.pept.transport.Connection;
+import org.mariadb.jdbc.*;
+
+import com.sun.glass.ui.Application;
+
+
 
 /**
  * Servlet implementation class S14Servlet01
@@ -32,8 +42,6 @@ public class S14Servlet01 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//database에서 record 가져오기
-		//1.연결설정
 		/*
 		 * String host= "본인 ip";
 		 *  String schema = "db테이블(w3schools)";
@@ -42,21 +50,49 @@ public class S14Servlet01 extends HttpServlet {
 		 * 
 		 * Connection con = null;
 		 */
-		// 2.statement 객체 생성
-		 //3.쿼리 생성
-		 //4.실행결과 정제
-		 //5.자원닫기
-		 
-		/*
-		 * List<String> cities= new ArrayList<>(); cities.add("Berlin");
-		 * cities.add("London"); cities.add("Madrid");
-		 */
-		//request에 record 넣기
-		request.setAttribute("cities", cities);
-		//jsp로 forward
-		String location = "/WEB-INF/view/chap14/ex01.jsp";
-		request.getRequestDispatcher(location).forward(request, response);
-	}
+		//JDBCListener 대체
+		//database에서 record 가져오기
+		ServletContext application = getServletContext();
+		// 1. 연결설정
+				DataSource ds = (DataSource) application.getAttribute("dbpool");
+
+				String sql = "SELECT city FROM Customers";
+				try (
+						Connection con = ds.getConnection();
+						// 2. statement 객체 생성
+						Statement stmt = con.createStatement();
+						// 3. 쿼리 실행
+						ResultSet rs = stmt.executeQuery(sql);) {
+
+					// 4. 실행결과 정제
+					while (rs.next()) {
+						String city = rs.getString(1);
+						System.out.println(city);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				// 5. 자원닫기
+				//		rs.close();
+				//		stmt.close();
+				//		con.close();
+
+				/*
+				List<String> cities = new ArrayList<>();
+				cities.add("Berlin");
+				cities.add("London");
+				cities.add("Madrid");
+				*/
+
+				// request에 records 넣기
+//				request.setAttribute("cities", cities);
+
+				// jsp로 forward
+				String location = "/WEB-INF/view/chap14/ex01.jsp";
+				request.getRequestDispatcher(location).forward(request, response);
+			}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
